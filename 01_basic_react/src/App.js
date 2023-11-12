750f5fae6627180a3dedefccac85f0a3ba4a40d2
abhishekmudhol <104548603+abhishekmudhol@users.noreply.github.com>
2023-11-12 21:43:11 +0530

    React Basics

---
+++ 01_basic_react/src/App.js
@@ -0,0 +1,13 @@
+import Learn from "./Learn";
+
+
+function App() {
+  return (   
+    <>
+    <Learn />
+    <h1>RAM</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01_basic_react/src/Learn.jsx
@@ -0,0 +1,9 @@
+//we can keep this file name with .js extention IT WORKS 
+
+function Learn(){   // TRY TO KEEP components NAME capitalize
+    return(
+        <h2>learning React</h2>
+    )
+}
+
+export default Learn
