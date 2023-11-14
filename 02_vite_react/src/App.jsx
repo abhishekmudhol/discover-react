b2e88efbead2791b28734fadba61300e7d7de28d
abhishekmudhol <104548603+abhishekmudhol@users.noreply.github.com>
2023-11-14 22:32:57 +0530

    vite

---
+++ 02_vite_react/src/App.jsx
@@ -0,0 +1,13 @@
+import Travel from "./travel"
+
+function App() {
+
+  return (
+    <>                 {/* IT IS CALLED fragment IN react */}
+    <Travel />
+    <h4> travel</h4>
+    </>
+  )
+}
+
+export default App
---
+++ 02_vite_react/src/travel.jsx
@@ -0,0 +1,8 @@
+
+function Travel() {
+    return(
+        <h2>Love for Technology AND thrill for Travelling</h2>
+    )
+}
+
+export default  Travel
